283687eabf6d7593002dbe6e864fbc85313cdf99
Matheus Pablo <mp123g@gmail.com>
2025-11-13 19:59:35 +0000

    Backup: Estrutura inicial do projeto (Skeleton)

---
+++ gerenciador-propostas/src/main/java/com/civies/gerenciadorpropostas/GerenciadorPropostasApplication.java
@@ -0,0 +1,13 @@
+package com.civies.gerenciadorpropostas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorPropostasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorPropostasApplication.class, args);
+	}
+
+}
