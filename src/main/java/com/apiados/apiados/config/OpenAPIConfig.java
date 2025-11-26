49b5f8dcb989c5f6f49c2617325aa8cdad98ce1b
barbara.oliveira4 <barbara.oliveira@mv.com.br>
2025-11-26 09:39:39 -0300

    adição de config para o swagger - barbara

---
+++ src/main/java/com/apiados/apiados/config/OpenAPIConfig.java
@@ -0,0 +1,33 @@
+package com.apiados.apiados.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI apiadosOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Apiados - Impacto Urbano Positivo") 
+                        .description("Esta API é o backend da plataforma **Apiados**.\n\n" +
+                                     "Aqui você encontra todos os endpoints para gerenciar o sistema de melhorias urbanas.\n" +
+                                     "**Funcionalidades:**\n" +
+                                     "- 👤 **Usuários:** Cadastro, login e perfil.\n" +
+                                     "- 🏙️ **Projetos:** Criação e listagem de demandas urbanas.\n" +
+                                     "- ⭐ **Votos:** Sistema de votação para priorizar melhorias.")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Equipe Apiados")
+                                .url("https://github.com/MatheusV2021/POO-PIUP")
+                                .email("contato@apiados.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org")));
+    }
+}
